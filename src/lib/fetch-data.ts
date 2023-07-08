0a51d40927907ff9cef94b7405e4b8b913e5f555
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-07-08 10:35:33 -0500

    update fetch function to return a proper error response and use try/catch

--- src/lib/fetch-data.ts
+++ src/lib/fetch-data.ts
@@ -1,11 +1,19 @@
-import type { FetchResponse } from '@/src/types/fetch';
+import type { FetchResponse, ErrorResponse } from '@/src/types/fetch';
 import axios from 'axios';
 
 const apiUrl = 'https://api.github.com/users/';
 
 const fetchUserData = async (username: string): Promise<FetchResponse> => {
-  const { data } = await axios.get<FetchResponse>(apiUrl + username);
-  return data;
+  try {
+    const { data } = await axios.get<FetchResponse>(apiUrl + username);
+    return data;
+  } catch (e: unknown) {
+    if (axios.isAxiosError(e)) {
+      const response = e.response?.data as ErrorResponse;
+      throw new Error(response.message);
+    }
+    throw new Error('Something went wrong');
+  }
 };
 
 export default fetchUserData;
